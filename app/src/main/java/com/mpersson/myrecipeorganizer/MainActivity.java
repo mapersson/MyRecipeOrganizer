7d553c0d8398d6ea8a1affd60362adf5689eef23
Inga Bilyk <inga11bil@gmail.com>
2021-03-23 16:40:48 -0700

    GridView Added

--- app/src/main/java/com/mpersson/myrecipeorganizer/MainActivity.java
+++ app/src/main/java/com/mpersson/myrecipeorganizer/MainActivity.java
@@ -3,12 +3,19 @@ package com.mpersson.myrecipeorganizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView gridView;
+    int [] pic = {R.drawable.recipe1, R.drawable.recipe2, R.drawable.recipe3, R.drawable.recipe4, R.drawable.recipe5, R.drawable.recipe6, R.drawable.recipe7, R.drawable.recipe8, R.drawable.recipe9, R.drawable.recipe10, R.drawable.recipe11, R.drawable.recipe12, R.drawable.recipe13, R.drawable.recipe14};
+    String[] foodNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView=(GridView)findViewById(R.id.gridViewId);
     }
 }
