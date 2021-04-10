e42cfb3d74295f0587e01ea08a8cc30244463350
mapersson <62900241+mapersson@users.noreply.github.com>
2021-04-10 14:14:02 -0600

    Recipe add functionality
    Added the ability to "parcel" a recipe
    Added the Add Recipe btn
    Added the return intent from the Add Recipe activity back to the Main Activity.

--- app/src/main/java/com/mpersson/myrecipeorganizer/AddRecipe.java
+++ app/src/main/java/com/mpersson/myrecipeorganizer/AddRecipe.java
@@ -3,10 +3,18 @@ package com.mpersson.myrecipeorganizer;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.mpersson.myrecipeorganizer.model.Recipe;
 
 public class AddRecipe extends AppCompatActivity {
+    public static final String NEW_RECIPE = "com.mpersson.myrecipeorganizer.NEW_RECIPE";
+    TextView mRecipeName;
+    TextView mRecipeDescription;
+    Recipe newRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +28,25 @@ public class AddRecipe extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+        mRecipeDescription = findViewById(R.id.text_recipe_description);
+        mRecipeName = findViewById(R.id.text_recipe_name);
     }
 
     public void btnSelectImage(View view) {
 
     }
+
+    public void btnAddRecipe(View view) {
+        String name = mRecipeName.getText().toString();
+        String description = mRecipeDescription.getText().toString();
+        newRecipe = new Recipe(name, description, null, null);
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(NEW_RECIPE, newRecipe);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
+    }
+
+
 }
